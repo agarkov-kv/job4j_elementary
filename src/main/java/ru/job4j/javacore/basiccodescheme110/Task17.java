be15ed2354a831ebc0cebf366dcd2104ac0ae4b7
agarkov-kv <Unisolll48@yandex.ru>
2024-01-22 14:51:17 +0300

    basiccodescheme_110_Task17

---
+++ src/main/java/ru/job4j/javacore/basiccodescheme110/Task17.java
@@ -0,0 +1,10 @@
+package ru.job4j.javacore.basiccodescheme110;
+
+public class Task17 {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x);
+        System.out.println((int) Math.pow(x, 2));
+        System.out.println((int) Math.pow(x, 3));
+    }
+}
