11cae13abca88046d5636b9cfc9f47ad012c960c
agarkov-kv <Unisolll48@yandex.ru>
2024-01-22 13:45:44 +0300

    basiccodescheme_110_Task12

---
+++ src/main/java/ru/job4j/javacore/basiccodescheme110/Task12.java
@@ -0,0 +1,9 @@
+package ru.job4j.javacore.basiccodescheme110;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println("Ping");
+        System.out.println("Pong");
+
+    }
+}
