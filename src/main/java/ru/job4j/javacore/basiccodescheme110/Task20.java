b0f0403af83d6e3f4fdce4fad1a5de0b905b5621
agarkov-kv <Unisolll48@yandex.ru>
2024-01-23 19:19:49 +0300

    asiccodescheme_110_Task20

---
+++ src/main/java/ru/job4j/javacore/basiccodescheme110/Task20.java
@@ -0,0 +1,14 @@
+package ru.job4j.javacore.basiccodescheme110;
+
+public class Task20 {
+    public static void print(int x, int y) {
+        System.out.println("Переменные: " + x + " " + y);
+        System.out.println("Сумма: " + (x + y));
+        System.out.println("Произведение: " + (x * y));
+    }
+
+    public static void main(String[] args) {
+        print(18, 19);
+
+    }
+}
