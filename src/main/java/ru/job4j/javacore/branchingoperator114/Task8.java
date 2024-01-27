93cda9161a96ac0dcd1e13f3808440a7fa8bdd6d
agarkov-kv <Unisolll48@yandex.ru>
2024-01-27 18:07:20 +0300

    8. Модуль числа

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task8.java
@@ -0,0 +1,8 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task8 {
+    public static void main(String[] args) {
+        double result = Math.abs(-5);
+        System.out.println(result);
+    }
+}
