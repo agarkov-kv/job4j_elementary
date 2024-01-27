78356dca92ece21331a59e809992722d2beacaca
agarkov-kv <Unisolll48@yandex.ru>
2024-01-27 17:32:20 +0300

    Task23

---
+++ src/main/java/ru/job4j/javacore/basiccodescheme110/Task22.java
@@ -0,0 +1,8 @@
+package ru.job4j.javacore.basiccodescheme110;
+
+public class Task22 {
+    public static void nameInsert(String name) {
+        String result = String.format("Привет, %s!", name);
+        System.out.println(result);
+    }
+}
