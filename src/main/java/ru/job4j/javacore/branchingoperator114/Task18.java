0fdbf300b1322b59e3ee73bf663dea55d003817d
agarkov-kv <Unisolll48@yandex.ru>
2024-01-28 15:47:12 +0300

    18. Сумма баллов под крышкой

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task18.java
@@ -0,0 +1,8 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task18 {
+    public static int capSum(int tens, int hundreds, int thousands) {
+        int sum = tens * 10 + hundreds * 100  + thousands * 1000;
+        return sum;
+    }
+}
