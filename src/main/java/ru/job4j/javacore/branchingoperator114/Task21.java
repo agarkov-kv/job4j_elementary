9f727b8d49847d9624ac6ea2e0e327d966f27ef5
agarkov-kv <Unisolll48@yandex.ru>
2024-01-28 16:09:56 +0300

    21. Разделить трехзначное число на отдельные цифры

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task21.java
@@ -0,0 +1,14 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task21 {
+    public static void separate(int number) {
+        int num1, num2, num3;
+        num1 =  number / 100;
+        num2 =  number / 10 % 10;
+        num3 =  number % 100 % 10;
+        System.out.println(num1 + " " + num2 + " " + num3);
+    }
+}
+// 116 / 100 = 1
+// 205 / 10  10 = 0
+// 116 % 100 % 10
