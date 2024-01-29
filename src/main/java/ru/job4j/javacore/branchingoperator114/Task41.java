41d084aed543bf7329862fe32f1fc307e9eec647
agarkov-kv <Unisolll48@yandex.ru>
2024-01-29 18:54:22 +0300

    41. "Сдвиг вправо по кругу" трехзначного числа

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task41.java
@@ -0,0 +1,12 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task41 {
+    public static void rightCircleShift(int number) {123
+        int num1 = number % 100 % 10 * 100;
+        int num2 = number / 100 * 10;
+        int num3 = number % 100 / 10;
+        int rightCircleShift = num1 + num2 + num3;
+        System.out.println(rightCircleShift);
+        //312
+    }
+}
