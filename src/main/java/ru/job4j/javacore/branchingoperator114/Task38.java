d97425b8495087eee1a723a0c87534e6e0693759
agarkov-kv <Unisolll48@yandex.ru>
2024-01-29 13:18:10 +0300

    38. "Линейный сдвиг влево" четырехзначного числа

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task38.java
@@ -0,0 +1,8 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task38 {
+    public static void leftShift(int number) {
+        int leftShift = number % 1000 * 10;
+        System.out.println(leftShift);
+    }
+}
