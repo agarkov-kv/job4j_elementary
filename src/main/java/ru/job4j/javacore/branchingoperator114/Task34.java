a72261cadc0eaf4c592042608bd5bb98bf9f920a
agarkov-kv <Unisolll48@yandex.ru>
2024-01-29 20:32:53 +0300

    34. Найти количество четных цифр в четырехзначном числе

--- src/main/java/ru/job4j/javacore/branchingoperator114/Task34.java
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task34.java
@@ -2,7 +2,7 @@ package ru.job4j.javacore.branchingoperator114;
 
 public class Task34 {
     public static void evenAmount(int number) {
-        int num1, num2, num3, num4, counter = 4;
+        int num1, num2, num3, num4, counter = 0;
         num1 = number / 1000 % 2;
         num2 = number % 1000 / 100 % 2;
         num3 = number % 1000 % 100 / 10 % 2;
