1b4c8afbf434a3e605003f26f368b0dc3886144d
agarkov-kv <Unisolll48@yandex.ru>
2024-01-30 12:28:40 +0300

    6. Проверка двух условий. Логическое И

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task6.java
@@ -0,0 +1,8 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task6 {
+        public static boolean check(int num) {
+            int ret = num % 2;
+            return ret == 0 && num > 0;
+        }
+}
