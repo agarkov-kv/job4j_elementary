376c14e85573abda7faded15836a88b865125c5d
agarkov-kv <Unisolll48@yandex.ru>
2024-01-30 11:24:23 +0300

    2. Первое больше второго

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task2.java
@@ -0,0 +1,8 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task2 {
+    public static boolean check(int first, int second) {
+        boolean check = first > second;
+        return check;
+    }
+}
