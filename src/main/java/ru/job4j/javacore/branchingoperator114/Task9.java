1ea8383f9b53084b7a6aa00565a9228ce009eb58
agarkov-kv <Unisolll48@yandex.ru>
2024-01-30 12:55:43 +0300

    9. Операторы & и &&. Операторы | и ||

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task9.java
@@ -0,0 +1,19 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task9 {
+    public static boolean showA() {
+        System.out.println("A");
+        return false;
+    }
+
+    public static boolean showB() {
+        System.out.println("B");
+        return false;
+    }
+
+    public static void main(String[] args) {
+        if (showA() && showB()) {
+
+        }
+    }
+}
