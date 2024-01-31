2072028638dbdf2a77578fcbeebdbf6f5c100f0e
agarkov-kv <Unisolll48@yandex.ru>
2024-01-31 19:00:57 +0300

    22. Вычислить, есть ли корни у квадратного уравнения

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task22.java
@@ -0,0 +1,13 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task22 {
+    public static void haveRoots(int a, int b, int c) {
+        int disco = b * b - 4 * a * c;
+        if (disco >= 0) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
+//
