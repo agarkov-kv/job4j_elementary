489debb111dc0e452e3be7838b99de34edf43ec7
agarkov-kv <Unisolll48@yandex.ru>
2024-01-31 19:44:51 +0300

    Является ли число положительным

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task25.java
@@ -0,0 +1,11 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task25 {
+    public static void isPositive(int number) {
+        if (number > 0) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
