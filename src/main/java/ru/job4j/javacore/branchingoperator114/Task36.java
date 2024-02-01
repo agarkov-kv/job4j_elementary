ee530f2679c3ed5f0743fc538fe5bdb17fd481ff
agarkov-kv <Unisolll48@yandex.ru>
2024-02-01 20:19:53 +0300

    36. Состоит ли двузначное число из одинаковых цифр

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task36.java
@@ -0,0 +1,13 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task36 {
+    public static void sameNums(int number) {
+        int num1 = number / 10;
+        int num2 = number % 10;
+        if (num1 == num2) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
