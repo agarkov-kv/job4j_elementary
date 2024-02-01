9fcbe2322a7fc0da8a7a61cdf5a51d2381599a73
agarkov-kv <Unisolll48@yandex.ru>
2024-02-01 20:44:47 +0300

    40. "Дважды четное" трехзначное число

--- src/main/java/ru/job4j/javacore/branchingoperator114/Task39.java
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task39.java
@@ -1,10 +1,11 @@
 package ru.job4j.javacore.branchingoperator114;
 
 public class Task39 {
-    public static void numsCompare(int number) {
-        int num1 = number / 10;
-        int num2 = number % 10;
-        if (num1 > num2) {
+    public static void isDoubleEven(int number) {
+        int num1 = number / 100;
+        int num2 = number % 100 / 10;
+        int num3 = number % 100 % 10;
+        if (number % 2 == 0 && (num1 + num2 + num3) % 2 == 0) {
             System.out.println("Да");
         } else {
             System.out.println("Нет");
