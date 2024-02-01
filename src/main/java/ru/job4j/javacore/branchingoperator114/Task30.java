7c77a02ac60958588f45878dab2bcace9d195231
agarkov-kv <Unisolll48@yandex.ru>
2024-02-01 18:51:57 +0300

    30. Изменить число в зависимости от его знака

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task30.java
@@ -0,0 +1,13 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task30 {
+    public static void numModify(int number) {
+        if (number < 0) {
+            System.out.println(number * -1);
+        } else if (number > 0) {
+            System.out.println(number * 0);
+        } else {
+            System.out.println(number);
+        }
+    }
+}
