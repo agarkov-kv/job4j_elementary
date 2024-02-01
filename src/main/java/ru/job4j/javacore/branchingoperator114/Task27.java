74f1535cd393c1a7dd085929cf0aded5ab9453d4
agarkov-kv <Unisolll48@yandex.ru>
2024-02-01 18:40:52 +0300

    28. Ученики и парты

--- src/main/java/ru/job4j/javacore/branchingoperator114/Task27.java
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task27.java
@@ -2,7 +2,7 @@ package ru.job4j.javacore.branchingoperator114;
 
 public class Task27 {
     public static void isEnough(int pupils, int chairs) {
-        if (chairs > pupils) {
+        if (pupils / 2 <= chairs || pupils == 0) {
             System.out.println("Да");
         } else {
             System.out.println("Нет");
