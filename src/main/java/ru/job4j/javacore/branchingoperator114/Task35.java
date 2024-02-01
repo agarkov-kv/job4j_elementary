91c831c6b6d033cb43952fd2f7177bafe850eb20
agarkov-kv <Unisolll48@yandex.ru>
2024-02-01 20:09:56 +0300

    35. Является ли число оценкой ученика

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task35.java
@@ -0,0 +1,11 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task35 {
+    public static void gradeCheck(int grade) {
+        if (grade >= 1 && grade <= 5) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
