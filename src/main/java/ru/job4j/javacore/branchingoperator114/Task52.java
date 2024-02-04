43c7d38ac9b31795439ac88ceb40af9ed3c46c97
agarkov-kv <Unisolll48@yandex.ru>
2024-02-04 20:01:40 +0300

    52. Равны ли дроби друг другу

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task52.java
@@ -0,0 +1,11 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task52 {
+    public static void isEquals(int a1, int a2, int b1, int b2) {
+        if (a1 * 1.0 / a2 == b1 * 1.0 / b2) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
