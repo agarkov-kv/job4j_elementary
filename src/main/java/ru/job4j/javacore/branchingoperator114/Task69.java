fb79f135414803b3d4a29a178ed849e67e9d2820
agarkov-kv <Unisolll48@yandex.ru>
2024-02-06 19:59:13 +0300

    69. Определить фигуру по длинам сторон

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task69.java
@@ -0,0 +1,11 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task69 {
+    public static void squareOrRectangle(int a, int b) {
+        if (a == b) {
+            System.out.println("Квадрат");
+        } else {
+            System.out.println("Прямоугольник");
+        }
+    }
+}
