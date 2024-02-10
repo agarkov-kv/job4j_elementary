5d1b2db95d4e85c52d3ab7cd025efaceef1ed289
agarkov-kv <Unisolll48@yandex.ru>
2024-02-10 08:55:33 +0300

    1. Вывести на консоль числа от 0 до N

---
+++ src/main/java/ru/job4j/javacore/loops115/Task1.java
@@ -0,0 +1,9 @@
+package ru.job4j.javacore.loops115;
+
+public class Task1 {
+    public static void out(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
