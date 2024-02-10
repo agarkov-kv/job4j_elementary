cfcb0e974969fd916392eee8712e17dcde362096
agarkov-kv <Unisolll48@yandex.ru>
2024-02-10 09:31:29 +0300

    3. Вывести на консоль числа от N до 0

---
+++ src/main/java/ru/job4j/javacore/loops115/Task3.java
@@ -0,0 +1,10 @@
+package ru.job4j.javacore.loops115;
+
+public class Task3 {
+    public static void out(int n) {
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
+//Выведите на консоль числа от N до 0. N не входит. В этом упражнении используйте цикл for c index
