1de5a24ff5bae88b95c41924f6ef899d5e608053
agarkov-kv <Unisolll48@yandex.ru>
2024-02-11 09:23:13 +0300

    4. Вывести на консоль числа от M до N.

---
+++ src/main/java/ru/job4j/javacore/loops115/Task4.java
@@ -0,0 +1,9 @@
+package ru.job4j.javacore.loops115;
+
+public class Task4 {
+    public static void out(int m, int n) {
+        for (int i = m; i > n; i--) {
+            System.out.println(i);
+        }
+    }
+}
