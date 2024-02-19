f3f0d91dbea054179007586b60fd39987759e0d4
agarkov-kv <Unisolll48@yandex.ru>
2024-02-19 08:04:39 +0300

    25. Вывести числа в арифметической прогрессии по возрастанию

--- src/main/java/ru/job4j/javacore/loops115/Task25.java
+++ src/main/java/ru/job4j/javacore/loops115/Task25.java
@@ -1,7 +1,15 @@
 package ru.job4j.javacore.loops115;
 
+import java.util.StringJoiner;
+
 public class Task25 {
     public static void loop(int num) {
-        //...Ваш код...;
+        StringJoiner numb = new StringJoiner(" ");
+        int number = 7;
+        for (int i = 0; i < num; i++) {
+            numb.add(String.valueOf(number));
+            number = number + 4;
+        }
+        System.out.println(numb);
     }
 }
