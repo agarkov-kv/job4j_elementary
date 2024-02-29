68c4825d7f8d28b00c0c129af5ce53ed60d0e485
agarkov-kv <Unisolll48@yandex.ru>
2024-02-29 11:40:44 +0300

    62. "Провальная" контрольная работа

---
+++ src/main/java/ru/job4j/javacore/loops115/Task62.java
@@ -0,0 +1,19 @@
+package ru.job4j.javacore.loops115;
+
+public class Task62 {
+    public static void loop(int amount, int[] grades) {
+        int count = 0;
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[i] <= 2) {
+                count++;
+            }
+        }
+        if (count >= amount - count) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+    //when5And32145
+    //2 и 3
+}
