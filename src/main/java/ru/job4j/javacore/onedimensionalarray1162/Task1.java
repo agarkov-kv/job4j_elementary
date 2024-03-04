ee2f41a4dd6522c225f56a5bd669e86f8b3e7996
agarkov-kv <Unisolll48@yandex.ru>
2024-03-04 11:00:30 +0300

    1. Вывести значения массива, начиная с первой ячейки-done

--- src/main/java/ru/job4j/javacore/onedimensionalarray1162/Task1.java
+++ src/main/java/ru/job4j/javacore/onedimensionalarray1162/Task1.java
@@ -1,4 +1,13 @@
 package ru.job4j.javacore.onedimensionalarray1162;
 
+import java.util.StringJoiner;
+
 public class Task1 {
+    public static void array(int[] arr) {
+        StringJoiner numb = new StringJoiner(" ");
+        for (int i = 0; i < arr.length; i++) {
+            numb.add(String.valueOf(arr[i]));
+        }
+        System.out.println(numb);
+    }
 }
