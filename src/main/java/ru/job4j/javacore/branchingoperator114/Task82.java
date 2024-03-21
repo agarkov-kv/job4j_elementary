7f85fe91eadb232cbb0bb82bd53757bb8fd9c792
KIRILL <unisolll48@yandex.ru>
2024-03-21 21:29:26 +0300

    82. Сравнить строки

---
+++ src/main/java/ru/job4j/javacore/branchingoperator114/Task82.java
@@ -0,0 +1,7 @@
+package ru.job4j.javacore.branchingoperator114;
+
+public class Task82 {
+    public static boolean isEquals(String first, String second) {
+        return first == second;
+    }
+}
