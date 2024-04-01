1a251c463e5cb6eb7fd7915d7b451dabed9cfaab
KIRILL <unisolll48@yandex.ru>
2024-04-01 15:33:43 +0300

    4. Индексы в массивах

---
+++ src/main/java/ru/job4j/javacore/array116/Task4.java
@@ -0,0 +1,13 @@
+package ru.job4j.javacore.array116;
+
+public class Task4 {
+    public static int getFirstElement(int[] array) {
+        int rsl = array[0];
+        return rsl;
+    }
+
+    public static int getLastElement(int[] array) {
+        int rsl = array[array.length - 1];
+        return rsl;
+    }
+}
