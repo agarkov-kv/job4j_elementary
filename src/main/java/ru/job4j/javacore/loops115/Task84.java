9827de52e699af989bc2cf7450445b506e72c553
KIRILL <unisolll48@yandex.ru>
2024-04-09 08:13:35 +0300

    96. Найти наилучший результат

--- src/main/java/ru/job4j/javacore/loops115/Task84.java
+++ src/main/java/ru/job4j/javacore/loops115/Task84.java
@@ -2,16 +2,5 @@ package ru.job4j.javacore.loops115;
 
 public class Task84 {
     public static void loop(int a1, int a2, int num) {
-        int sum = 0;
-        int a3 = a1 + a2;
-        for (int i = 1; i < num; i++) {
-            sum = sum + a3;
-        }
-        System.out.println(sum);
     }
 }
-
-// a1 = 5
-// a2 = 8
-// num = 3
-// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
